00acb593ea9729900db1e401db340ae150c3896b
Bhavethra <bhavethra@Mac-252.lan>
2026-03-25 22:46:37 -0400

    fixx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ import {
   addDoc,
   setDoc,
 } from 'firebase/firestore';
-import Login from '.Login.jsx';
+import Login from './login';
 import { QRCodeCanvas } from 'qrcode.react';
 
 function App() {
